19472e4a596fc84d8e776efcfc8c90c59369d434
junemonash <126083693+junemonash@users.noreply.github.com>
2023-05-21 11:16:33 +0800

    Assignment 2 Task 2 Done.

--- ratio.py
+++ ratio.py
@@ -9,16 +9,26 @@ I = TypeVar("I")
 class Percentiles(Generic[T]):
 
     def __init__(self) -> None:
-        raise NotImplementedError()
-    
+        self.BST = BinarySearchTree()
+        
     def add_point(self, item: T):
-        raise NotImplementedError()
-    
+        self.BST[item] = None
+        
+        
     def remove_point(self, item: T):
-        raise NotImplementedError()
+        del self.BST[item]
 
-    def ratio(self, x, y):
-        raise NotImplementedError()
+    def ratio(self, x, y): # 13, 10
+        x_ratio = ceil(x / 100 * len(self.BST)) + 1
+        y_ratio = len(self.BST) - ceil(y / 100 * len(self.BST))
+
+        temp_list = []
+
+        for i in range(x_ratio,y_ratio + 1):
+            temp_list.append(self.BST.kth_smallest(i,self.BST.root).key)
+
+        return temp_list
+        
 
 if __name__ == "__main__":
     points = list(range(50))
