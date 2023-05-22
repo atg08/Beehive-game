2742fd2a9c0334f8e544426b95c7b182a02f0307
AdittiGupta <agup0031@student.monash.edu>
2023-05-22 16:28:24 +0800

    draft TASK3

--- threedeebeetree.py
+++ threedeebeetree.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from typing import Generic, TypeVar, Tuple
 from dataclasses import dataclass, field
+from referential_array import ArrayR
 
 I = TypeVar('I')
 Point = Tuple[int, int, int]
@@ -8,12 +9,69 @@ Point = Tuple[int, int, int]
 @dataclass
 class BeeNode:
 
+    
     key: Point
-    item: I
+    my_child : ArrayR[BeeNode] 
+    item: I = None
+    
+    """
+    my_child[0] = top north-east (+++)
+    my_child[1] = top north-west (-++)
+    my_child[2] = top south-east (+-+)
+    my_child[3] = top south-west (--+)
+    my_child[4] = bot north-east (++-)
+    my_child[5] = bot north-west (-+-)
+    my_child[6] = bot south-east (+--)
+    my_child[7] = bot south-west (---)
+    """
     subtree_size: int = 1
 
+    def __init__(self, key : Point, item : I) -> None:
+          self.key = key
+          self.item = item
+          self.my_child = ArrayR(length = 8)
+          for i in range (len(self.my_child)):
+                self.my_child[i] = None
+                
+
     def get_child_for_key(self, point: Point) -> BeeNode | None:
-        raise NotImplementedError()
+        print("point is " , point)
+        for child in range (len(self.my_child)):
+            if self.my_child[child] != None:
+                match child:
+                    case 0:
+                            if point[2] >= self.key[2] and point[1] >= self.key[1] and point[0] >= self.key[0]:
+                                print("\nmy child is " , self.my_child[child].key)
+                                return self.my_child[child]
+                    case 1:
+                            if point[2] >= self.key[2] and point[1] >= self.key[1] and point[0] < self.key[0]:
+                                print("\nmy child is " , self.my_child[child].key)
+                                return self.my_child[child]
+                    case 2:
+                            if point[2] >= self.key[2] and point[1] < self.key[1] and point[0] >= self.key[0]:
+                                print("\nmy child is " , self.my_child[child].key)
+                                return self.my_child[child]
+                    case 3:
+                            if point[2] >= self.key[2] and point[1] < self.key[1] and point[0] < self.key[0]:
+                                print("\nmy child is " , self.my_child[child].key)
+                                return self.my_child[child]
+                    case 4:
+                            if point[2] < self.key[2] and point[1] >= self.key[1] and point[0] >= self.key[0]:
+                                print("\nmy child is " , self.my_child[child].key)
+                                return self.my_child[child]
+                    case 5:
+                            if point[2] < self.key[2] and point[1] >= self.key[1] and point[0] < self.key[0]:
+                                print("\nmy child is " , self.my_child[child].key)
+                                return self.my_child[child]
+                    case 6:
+                            if point[2] < self.key[2] and point[1] < self.key[1] and point[0] >= self.key[0]:
+                                print("\nmy child is " , self.my_child[child].key)
+                                return self.my_child[child]
+                    case 7:
+                            if point[2] < self.key[2] and point[1] < self.key[1] and point[0] < self.key[0]:
+                                print("\nmy child is " , self.my_child[child].key)
+                                return self.my_child[child]                          
+        return None
 
 
 class ThreeDeeBeeTree(Generic[I]):
@@ -23,7 +81,7 @@ class ThreeDeeBeeTree(Generic[I]):
         """
             Initialises an empty 3DBT
         """
-        self.root = None
+        self.root : BeeNode|None = None
         self.length = 0
 
     def is_empty(self) -> bool:
@@ -64,7 +122,75 @@ class ThreeDeeBeeTree(Generic[I]):
         """
             Attempts to insert an item into the tree, it uses the Key to insert it
         """
-        raise NotImplementedError()
+
+        #temp_node : BeeNode = None
+        if current is None:
+            current = BeeNode(key = key , item = item)
+            print("current id " , id(current.my_child))
+            self.length += 1
+        elif key[2] >= current.key[2] and key[1] >= current.key[1] and key[0] >= current.key[0]: 
+                    current.my_child[0] = self.insert_aux(current = current.my_child[0], key = key, item = item)
+                    current.subtree_size += 1
+        elif key[2] >= current.key[2] and key[1] >= current.key[1] and key[0] < current.key[0]: 
+                    current.my_child[1] = self.insert_aux(current.my_child[1], key, item)
+                    # current.my_child[1] = temp_node
+                    current.subtree_size += 1
+        elif key[2] >= current.key[2] and key[1] < current.key[1] and key[0] >= current.key[0]: 
+                    current.my_child[2] = self.insert_aux(current = current.my_child[2], key = key, item = item) 
+                    current.subtree_size += 1 
+        elif key[2] >= current.key[2] and key[1] < current.key[1] and key[0] < current.key[0]: 
+                    current.my_child[3] = self.insert_aux(current = current.my_child[3], key = key, item = item)
+                    current.subtree_size += 1 
+        elif key[2] < current.key[2] and key[1] >= current.key[1] and key[0] >= current.key[0]: 
+                    current.my_child[4] = self.insert_aux(current = current.my_child[4], key = key, item = item) 
+                    current.subtree_size += 1
+        elif key[2] < current.key[2] and key[1] >= current.key[1] and key[0] < current.key[0]: 
+                    current.my_child[5] = self.insert_aux(current = current.my_child[5], key = key, item = item)
+                    current.subtree_size += 1
+        elif key[2] < current.key[2] and key[1] < current.key[1] and key[0] >= current.key[0]: 
+                    current.my_child[6] = self.insert_aux(current = current.my_child[6], key = key, item = item)
+                    current.subtree_size += 1
+        elif key[2] < current.key[2] and key[1] < current.key[1] and key[0] < current.key[0]: 
+                    current.my_child[7] = self.insert_aux(current = current.my_child[7], key = key, item = item)
+                    current.subtree_size += 1
+        else:
+            return ValueError("Key already exists")
+        return current
+    
+        # if current is None:
+        #     current = BeeNode(key = key , item = item)
+        # if key[2] > current.key[2]:
+        #     if key[1] > current.key[1]:
+        #         if key[0] > current.key[0]: 
+        #             current.my_child[0] = self.insert_aux(current = current.my_child[0], key = key, item = item)
+        #         else:
+        #             current.my_child[1] = self.insert_aux(current = current.my_child[1], key = key, item = item)
+        #     else:
+        #         if key[0] > current.key[0]:
+        #             current.my_child[2] = self.insert_aux(current = current.my_child[2], key = key, item = item)
+        #         else:
+        #             current.my_child[3] = self.insert_aux(current = current.my_child[3], key = key, item = item)
+        # elif key[2] < current.key[2]:
+        #     if key[1] > current.key[1]:
+        #         if key[0] > current.key[0]:
+        #             current.my_child[4] = self.insert_aux(current = current.my_child[4], key = key, item = item)
+        #         else:
+        #             current.my_child[5] = self.insert_aux(current = current.my_child[5], key = key, item = item)
+        #     else:
+        #         if key[0] > current.key[0]:
+        #             current.my_child[6] = self.insert_aux(current = current.my_child[6], key = key, item = item)
+        #         else:
+        #             current.my_child[7] = self.insert_aux(current = current.my_child[7], key = key, item = item)
+        # else:
+        #     return ValueError("key already exists")
+        # return current
+
+
+
+
+
+
+        # raise NotImplementedError()
 
     def is_leaf(self, current: BeeNode) -> bool:
         """ Simple check whether or not the node is a leaf. """
