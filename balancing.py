cc790b6bfe787c63123ebc104b44ba602b915e3c
AdittiGupta <agup0031@student.monash.edu>
2023-05-22 20:39:33 +0800

    TASK5 done

--- balancing.py
+++ balancing.py
@@ -1,5 +1,15 @@
 from __future__ import annotations
 from threedeebeetree import Point
+from ratio import *
+from algorithms.mergesort import mergesort
 
 def make_ordering(my_coordinate_list: list[Point]) -> list[Point]:
+    # percentile_obj = Percentiles()
+    # temp_sorted_list = []
+    # for i in range (len())
+    
+
+    # temp_sorted_list = mergesort(l = my_coordinate_list, key = lambda x : x[0])
+
+
     raise NotImplementedError()
--- beehive.py
+++ beehive.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from dataclasses import dataclass
 from heap import MaxHeap
 
@@ -13,16 +14,37 @@ class Beehive:
     nutrient_factor: int
     volume: int = 0
 
+
+    def __gt__(self , other : Beehive) -> bool:
+        return self.nutrient_factor * min(self.capacity , self.volume) > other.nutrient_factor * min(other.capacity , other.volume)
+
+    def __le__(self , other : Beehive) -> bool:
+        return self.nutrient_factor * min(self.capacity , self.volume) <= other.nutrient_factor * min(other.capacity , other.volume)
+
 class BeehiveSelector:
 
-    def __init__(self, max_beehives: int):
-        raise NotImplementedError()
+    def __init__(self, max_beehives: int) -> None:
+        self.my_beehive : MaxHeap[Beehive] = MaxHeap(max_size = max_beehives)
 
-    def set_all_beehives(self, hive_list: list[Beehive]):
-        raise NotImplementedError()
+    def set_all_beehives(self, hive_list: list[Beehive]) -> None:
+        self.my_beehive.length = 0
+        for i in range (len(hive_list)):
+            self.my_beehive.add(hive_list[i])
     
-    def add_beehive(self, hive: Beehive):
-        raise NotImplementedError()
+    def add_beehive(self, hive: Beehive) -> None:
+        self.my_beehive.add(element = hive)
     
-    def harvest_best_beehive(self):
-        raise NotImplementedError()
+    def harvest_best_beehive(self) -> float:
+        max_behive = self.my_beehive.get_max()
+        if max_behive.capacity > max_behive.volume:
+            new_volume = 0
+        else:
+            new_volume = max_behive.volume - max_behive.capacity
+
+        new_behive : Beehive = Beehive(x = max_behive.x , y = max_behive.y , z = max_behive.z , capacity = max_behive.capacity, nutrient_factor = max_behive.nutrient_factor , volume = new_volume)
+
+        self.add_beehive(hive = new_behive)
+
+        return max_behive.nutrient_factor * min(max_behive.capacity , max_behive.volume)
+    
+  
