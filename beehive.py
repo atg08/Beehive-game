563d14efd2d717c0b6c80643dfae7df8bf79d800
junemonash <126083693+junemonash@users.noreply.github.com>
2023-05-23 16:17:30 +0800

    Task 5 Done.

--- beehive.py
+++ beehive.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from dataclasses import dataclass
 from heap import MaxHeap
 
@@ -13,16 +14,34 @@ class Beehive:
     nutrient_factor: int
     volume: int = 0
 
+    def __gt__(self , other : Beehive) -> bool:
+        return self.nutrient_factor * min(self.capacity , self.volume) > other.nutrient_factor * min(other.capacity , other.volume)
+
+    def __le__(self , other : Beehive) -> bool:
+        return self.nutrient_factor * min(self.capacity , self.volume) <= other.nutrient_factor * min(other.capacity , other.volume)
+
 class BeehiveSelector:
 
     def __init__(self, max_beehives: int):
-        raise NotImplementedError()
+        self.cur_hive : MaxHeap = MaxHeap(max_beehives)
 
     def set_all_beehives(self, hive_list: list[Beehive]):
-        raise NotImplementedError()
+        self.cur_hive.length = 0
+        for i in range (len(hive_list)):
+            self.cur_hive.add(hive_list[i])
     
     def add_beehive(self, hive: Beehive):
-        raise NotImplementedError()
+        self.cur_hive.add(element = hive)
     
     def harvest_best_beehive(self):
-        raise NotImplementedError()
+        gt_hive = self.cur_hive.get_max()
+        if gt_hive.capacity > gt_hive.volume:
+            new_volume = 0
+        else:
+            new_volume = gt_hive.volume - gt_hive.capacity
+
+        new_hive = Beehive(x= gt_hive.x, y=gt_hive.y, z=gt_hive.z, capacity = gt_hive.capacity, nutrient_factor = gt_hive.nutrient_factor ,volume = new_volume)
+
+        self.add_beehive(hive = new_hive)
+
+        return gt_hive.nutrient_factor * min(gt_hive.capacity,gt_hive.volume)
