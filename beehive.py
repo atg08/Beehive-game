583b430bc73eebf5d85f7f5def05614d73552ae6
junemonash <126083693+junemonash@users.noreply.github.com>
2023-05-28 18:03:29 +0800

    Started documentation time-complexity

--- beehive.py
+++ beehive.py
@@ -15,9 +15,33 @@ class Beehive:
     volume: int = 0
 
     def __gt__(self , other : Beehive) -> bool:
+        """
+            Check which nutrient_factor is greater between self and other.
+
+            Args:
+            - other : other beehive for compare.
+
+            Returns:
+            - it returns greater nutrient_factor.
+
+            Complexity:
+            - Best & Worst case : O(1)
+        """
         return self.nutrient_factor * min(self.capacity , self.volume) > other.nutrient_factor * min(other.capacity , other.volume)
 
     def __le__(self , other : Beehive) -> bool:
+        """
+            Check which nutrient_factor is lesser or equal between self and other.
+
+            Args:
+            - other : other beehive for compare.
+
+            Returns:
+            - it returns smaller or equal nutrient_factor.
+
+            Complexity:
+            - Best & Worst case : O(1)
+        """
         return self.nutrient_factor * min(self.capacity , self.volume) <= other.nutrient_factor * min(other.capacity , other.volume)
 
 class BeehiveSelector:
@@ -26,14 +50,46 @@ class BeehiveSelector:
         self.my_beehive : MaxHeap[Beehive] = MaxHeap(max_size = max_beehives)
 
     def set_all_beehives(self, hive_list: list[Beehive]) -> None:
+        """
+            Replace all current (possibly none) beehives in the selector
+             with the beehives in the list provided as an argument.
+
+            Args:
+            - other : other beehive for compare.
+
+            Returns:
+            - it returns smaller or equal nutrient_factor.
+
+            Complexity:
+            - Best case & Worst case : O(M log M), M is len(hive_list)
+        """
         self.my_beehive.length = 0
         for i in range (len(hive_list)):
             self.my_beehive.add(hive_list[i])
     
     def add_beehive(self, hive: Beehive) -> None:
+        """
+            Add beehive in BeehiveSelector.
+
+            Args:
+            - hive : Beehive
+
+            Complexity:
+            - Best & Worst case : O(log N), N : the number of beehives.
+        """
         self.my_beehive.add(element = hive)
     
     def harvest_best_beehive(self) -> float:
+        """
+            Harvest with finding minimum between honey capacity and honey volume.
+
+            Returns:
+            - it will return the number of emeralds, 
+              which is calculated with nutrient_factor * minimum value between capacity and volume.
+
+            Complexity:
+            - Best case & Worst case : O(log N), N : the number of beehives.
+        """
         max_behive = self.my_beehive.get_max()
         if max_behive.capacity > max_behive.volume:
             new_volume = 0
